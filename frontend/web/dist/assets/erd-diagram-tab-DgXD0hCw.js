b77c8734203d2adf1aa575bfbe953905017e4e80
NightOwl <fitensonwaynchiff031010@gmail.com>
2025-10-02 18:59:42 +0800

    Fix cannot build frontend bundle

--- frontend/web/dist/assets/erd-diagram-tab-DgXD0hCw.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./main-ClN3zn8I.js";function i(){return r.jsx("div",{children:"This is ERD Diagram Tab"})}export{i as default};
--- frontend/web/dist/assets/useAppSelector-B2O_wUyw.js
+++
@@ -1,1 +0,0 @@
-import{D as e}from"./main-ClN3zn8I.js";const r=e;export{r as u};
